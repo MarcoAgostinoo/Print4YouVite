44588d8f3a3ce59fa1554032c4be82140b5baa92
Marco Antonio <114794163+MarcoAgostinoo@users.noreply.github.com>
2024-06-28 01:37:43 -0300

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,1 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers"; // Import your root reducer
 
-const store = createStore(rootReducer); // Create your Redux store with rootReducer
-
-export default store;
